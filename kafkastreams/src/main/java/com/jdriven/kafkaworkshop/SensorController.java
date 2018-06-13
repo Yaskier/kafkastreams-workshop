f376180efa6df442d2467fe57877838ba0b6f8da
yaskier <r.ruiz.giner@gmail.com>
2018-06-13 09:59:27 +0200

    exercise 1 done

--- kafkastreams/src/main/java/com/jdriven/kafkaworkshop/SensorController.java
+++ kafkastreams/src/main/java/com/jdriven/kafkaworkshop/SensorController.java
@@ -2,6 +2,8 @@ package com.jdriven.kafkaworkshop;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +13,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class SensorController {
 
+    @Autowired private KafkaTemplate<String, SensorData> kafkaTemplate;
     private static final Logger LOGGER = LoggerFactory.getLogger(SensorController.class);
+    private String topic = TopicNames.RECEIVED_SENSOR_DATA;
 
     @GetMapping("/sensor")
     public String greetingForm(Model model) {
@@ -21,7 +25,7 @@ public class SensorController {
 
     @PostMapping("/sensor")
     public String sensorSubmit(@ModelAttribute SensorData sensorData) {
-        LOGGER.info("TODO submit data with spring kafkaTemplate");
+        kafkaTemplate.send(topic, sensorData.getId(), sensorData);
         return "sensor";
     }
 
